f71d515e3687cdfe1fdddb099d3d59481f730b37
Slav9ln1n <vaceslavdratov6@gmail.com>
2025-05-07 20:07:29 +0300

    Add all files
    Добавил все файлы проекта

---
+++ micro_sсript.js
@@ -0,0 +1,8 @@
+function present() {
+    const flower = document.querySelector(".flower");
+    const flower_2 = document.querySelector(".second_flower");
+    const minibro = document.querySelector(".minibro");
+    flower.src = "flower.png";
+    flower_2.src = "flower_2.png";
+    minibro.src = "minibro_active.jpg";
+}
